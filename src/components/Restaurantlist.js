7bf733d626c55f56e00ccee251ce4c30d810c012
sreerag217001 <sreeragp217001@gmail.com>
2023-02-09 00:50:21 -0800

    Restaurant

--- src/components/Restaurantlist.js
+++ src/components/Restaurantlist.js
@@ -2,29 +2,38 @@ import React, { useEffect } from 'react'
 import { useState } from 'react'
 import Restcard from './Restcard'
 import Row from 'react-bootstrap/Row';
-
+import { RestListAction } from '../actions/restactionlist';
+import { useDispatch, useSelector } from 'react-redux';
 
 function Restaurantlist() {
-    const[restaurantlist,setRestaurantlist]=useState([])
+    // const[restaurantlist,setRestaurantlist]=useState([])
 
     //to create a function for api call
-    const getRestaurant=async()=>{
-        //async await
-        await fetch('/restaurants.json')//async call
-        .then((data)=>data.json()
-          .then((result)=>setRestaurantlist(result.restaurants))
-        )
-      }
-        console.log(restaurantlist);
+    // const getRestaurant=async()=>{
+    //     //async await
+    //     await fetch('/restaurants.json')//async call
+    //     .then((data)=>data.json()
+    //       .then((result)=>setRestaurantlist(result.restaurants))
+    //     )
+    //   }
+        // console.log(restaurantlist);
+
+        const dispatch=useDispatch()
+
+        const result=useSelector(state=>state.restReducer)
+        console.log(result);//resrList:Array(10)
+
+        
 
     useEffect(()=>{
-      getRestaurant()
+    //   getRestaurant()
+    dispatch(RestListAction())
     },[])
    
   return (
     <Row>
         {
-           restaurantlist.map(item=>(
+           result.restList.map((item)=>(
             <Restcard restaurant={item}/>
           ))
         }
